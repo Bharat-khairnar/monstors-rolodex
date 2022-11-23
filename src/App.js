b397616243ef955195e0510aaad89926c3ef2771
bharat-khairnar <bharat.khairnar@bacancy.com>
2022-11-23 17:47:29 +0530

    Upload the project monstors-rolode

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { CardList } from './components/card-list/card-list.component';
+import { SearchBox } from './components/search-box/search-box.component';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+      super();
+
+      this.state = {
+          string: "My name is Bharat",
+          monstors: [],
+          searchField: ""
+      }
+
+      // this.handleChange = this.handleChange.bind(this)
+  }
+
+  componentDidMount() { // Fetching backend api data and put them into the setState for re-rendering
+      fetch('https://jsonplaceholder.typicode.com/users')
+          .then(response => response.json())
+          .then(users => this.setState( {monstors: users} ));
+  }
+
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  }
+
+
+  render() { 
+
+      const { monstors, searchField } = this.state;
+      const filterMonstors = monstors.filter(monstor =>
+         monstor.name.toLowerCase().includes(searchField.toLowerCase()) 
+      );
+      return (
+          <div className="App">
+            <header className="App-header">
+              <h1>Monstors Rolodex</h1>
+              <SearchBox placeholder="Search Monstors" handleChange={this.handleChange}/>
+              <CardList monstors={filterMonstors} />
+              <p>{ this.state.string }</p>
+              <button onClick={ () => this.setState( { string: "Hello may name is Nishi"})}>Chnage text</button>
+              
+              <p>
+                Edit <code>src/App.js</code> and save to reload.
+              </p>
+              <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                Learn React
+              </a>
+            </header>
+          </div>
+        );
+  }
 }
+// function App() {
+//   return (
+//     <div className="App">
+//       <Basic />
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Edit <code>src/App.js</code> and save to reload.
+//         </p>
+//         <a
+//           className="App-link"
+//           href="https://reactjs.org"
+//           target="_blank"
+//           rel="noopener noreferrer"
+//         >
+//           Learn React
+//         </a>
+//       </header>
+//     </div>
+//   );
+// }
 
 export default App;
